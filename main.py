8273b6f24f610da9bb7d109c5b489ee6e9f76387
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-02-02 11:28:28 +0100

    remove lock video button once pressed

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ from Downloader import Downloader
 
 def load_frontend():
     tab1, tab2 = st.tabs(["Audio Only", "Audio + Video(N/A)"])
-    video_url = st.text_input("Url")
-    run_button = st.button("Lock video")
+    form = st.empty()
+    with form:
+        with st.form("test_form"):
+            video_url = st.text_input("Url")
+            run_button = st.form_submit_button("Lock video")
 
     with tab1: #Audio Only
         if run_button:
             try:
-                print(video_url)
                 downloader = Downloader(video_url)
                 audio_stream = downloader.get_audio_stream()
                 st.image(f"{downloader.thumbnail_url}", caption=f"{downloader.title}", width=300)
                 download_button = st.button("Download")
+                form.empty()
                 if download_button:
                     audio_stream.download(output_path='./downloads')
             except RegexMatchError:
