8cc1f5828f79a58a66ddc2cc00c79d6fe71cc70e
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-02-02 11:27:21 +0100

    implement download funciton

--- Downloader.py
+++ Downloader.py
@@ -1,7 +1,6 @@
 from pytube import YouTube
 
-class Downloader(YouTube):
 
-        
+class Downloader(YouTube):
     def get_audio_stream(self):
         return self.streams.get_audio_only()
--- main.py
+++ main.py
@@ -5,13 +5,18 @@ from Downloader import Downloader
 def load_frontend():
     tab1, tab2 = st.tabs(["Audio Only", "Audio + Video(N/A)"])
     video_url = st.text_input("Url")
-    run_button = st.button("Download")
+    run_button = st.button("Lock video")
 
     with tab1: #Audio Only
         if run_button:
             try:
+                print(video_url)
                 downloader = Downloader(video_url)
-                streams = downloader.get_audio_stream().download(output_path='./downloads')
+                audio_stream = downloader.get_audio_stream()
+                st.image(f"{downloader.thumbnail_url}", caption=f"{downloader.title}", width=300)
+                download_button = st.button("Download")
+                if download_button:
+                    audio_stream.download(output_path='./downloads')
             except RegexMatchError:
                 st.warning('Incorrect Url', icon="⚠️")
 
